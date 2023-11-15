90780df6db440b70bdf0d0a49ee2b1f08021fd1f
HamsterStudent <hwanyamyu@gmail.com>
2023-11-15 18:19:24 +0900

    :art: Format : format components style

---
+++ src/app/_components/mainComponentsStyle.tsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export interface IProdData {
+  data: { name: string; img: string; price: number }[];
+  title?: string;
+}
+
+export const CategoryList = styled.ul`
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  background-color: #fff;
+  li {
+    width: 75px;
+    height: 32px;
+  }
+`;
+
+export const Info = styled.p`
+  padding: 7px;
+  background-color: #e6e7e7;
+  border-radius: 30px;
+  margin: 20px 10px 0 10px;
+  font-size: 0.7rem;
+  font-family: "UhBeeSe_hyun";
+`;
+
+export const Banner = styled.div`
+  width: 100%;
+  img {
+    width: 100%;
+    height: 100%;
+    position: relative !important;
+    object-fit: contain;
+  }
+`;
