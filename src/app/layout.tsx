ffe4103df3503de03684eb707d9cec85de883ab9
HamsterStudent <hwanyamyu@gmail.com>
2023-11-15 18:13:22 +0900

    :sparkles: Feat : add media query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 "use client";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import styled, { ThemeProvider, createGlobalStyle } from "styled-components";
 import { defaultTheme } from "@/style/theme";
 import StyledComponentsRegistry from "./lib/registry";
 import Head from "./head";
+import Header from "./_components/header";
+import { useIsMobile } from "./hooks/useIsMobile";
 
 const GlobalStyle = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
@@ -80,20 +82,99 @@ p, li, h1, h2, h3{
   transform : rotate(0.04deg); 
 }
 `;
+const Background = styled.section`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  --dot-bg: #fff5e3;
+  --dot-color: #cc7777;
+  --dot-size: 2px;
+  --dot-space: 40px;
+  background: linear-gradient(
+        90deg,
+        var(--dot-bg) calc(var(--dot-space) - var(--dot-size)),
+        transparent 1%
+      )
+      center / var(--dot-space) var(--dot-space),
+    linear-gradient(
+        var(--dot-bg) calc(var(--dot-space) - var(--dot-size)),
+        transparent 1%
+      )
+      center / var(--dot-space) var(--dot-space),
+    var(--dot-color);
+`;
+
+const AppWrap = styled.div`
+  width: 430px;
+  max-height: 914px;
+  height: 100%;
+  padding: 20px;
+  background-color: #646464;
+  border-radius: 20px;
+  border: solid 0.7px;
+  box-sizing: border-box;
+`;
+
+const AppInner = styled.div`
+  background-color: #fff;
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  .mainImg {
+    width: 100%;
+    img {
+      width: 100%;
+      height: 100%;
+      position: relative !important;
+      object-fit: contain;
+    }
+  }
+`;
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const isMobile = useIsMobile();
   return (
     <html lang="kr">
       <body>
         <StyledComponentsRegistry>
           <ThemeProvider theme={defaultTheme}>
             <GlobalStyle />
-            <Head />
-            <div>{children}</div>
+            <Background>
+              {isMobile ? (
+                <AppInner>
+                  <Head />
+                  <Header />
+                  <div>{children}</div>
+                </AppInner>
+              ) : (
+                <AppWrap>
+                  <AppInner>
+                    <Head />
+                    <Header />
+                    <div>{children}</div>
+                  </AppInner>
+                </AppWrap>
+              )}
+              {/* <AppWrap>
+                <AppInner>
+                  <Head />
+                  <Header />
+                  <div>{children}</div>
+                </AppInner>
+              </AppWrap> */}
+            </Background>
           </ThemeProvider>
         </StyledComponentsRegistry>
       </body>
