773fda57788b8db6dade876d4e3f33fb4045b124
HamsterStudent <hwanyamyu@gmail.com>
2023-11-22 12:29:02 +0900

    :label: Update : update initialState types
    update initialState and remove any type

--- src/redux/slices/cartSlice.ts
+++ src/redux/slices/cartSlice.ts
@@ -3,15 +3,36 @@ import Cookies from "js-cookie";
 
 interface IInitialState {
   loading: boolean;
-  cartItems: any;
-  shippingAddress: any;
+  showSlidbar: boolean;
+  cartItems: {
+    countInStock: number;
+    description: string;
+    id: string;
+    img: string;
+    name: string;
+    numReviews: number;
+    price: number;
+    qty: number;
+    rating: number;
+  }[];
+  shippingAddress: {};
+  paymentMethod: string;
+  itemsPrice: string;
+  shippingPrice: number;
+  totalPrice: string;
 }
 
 const initialState: IInitialState = Cookies.get("cart")
   ? { ...JSON.parse(Cookies.get("cart")!), loading: true, showSidebar: false }
-  : { loading: true, showSlidbar: false, cartItems: [], shippingAddress: {} };
+  : {
+      loading: true,
+      showSlidbar: false,
+      cartItems: [],
+      shippingAddress: {},
+      paymentMethod: "",
+    };
 
-const addDecimals = (num: any) => {
+const addDecimals = (num: number) => {
   return (Math.round(num * 100) / 100).toFixed(2);
 };
 
@@ -19,52 +40,48 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addToCart: (state: any, action: any) => {
+    addToCart: (state, action) => {
       const item = action.payload;
-      const existItem = state.cartItems.find((x: any) => x.id === item.id);
+      const existItem = state.cartItems.find((x) => x.id === item.id);
       if (existItem) {
-        state.cartItems = state.cartItems.map((x: any) =>
+        state.cartItems = state.cartItems.map((x) =>
           x.id === existItem.id ? item : x,
         );
       } else {
         state.cartItems = [...state.cartItems, item];
       }
       state.itemsPrice = addDecimals(
-        state.cartItems.reduce(
-          (acc: any, item: any) => acc + item.price * item.qty,
-          0,
-        ),
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0),
       );
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 100);
-      state.taxPrice = addDecimals(Number((0.15 * state.itemPrice).toFixed(2)));
+      state.shippingPrice = 3000;
+      // state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
       state.totalPrice = addDecimals(
-        Number(state.itemPrice) +
-          Number(state.shippingPrice) +
-          Number(state.taxPrice),
+        Number(state.itemsPrice) + Number(state.shippingPrice),
+        // 세금
+        // + Number(state.taxPrice),
       );
       Cookies.set("cart", JSON.stringify(state));
     },
-    removeFromCart: (state: any, action) => {
-      state.cartItems = state.cartItems.filter(
-        (x: any) => x.id !== action.payload,
-      );
+    removeFromCart: (state, action) => {
+      state.cartItems = state.cartItems.filter((x) => x.id !== action.payload);
       state.itemsPrice = addDecimals(
-        state.cartItems.reduce(
-          (acc: any, item: any) => acc + item.price * item.qty,
-          0,
-        ),
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0),
       );
-      state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 100);
-      state.taxPrice = addDecimals(Number((0.15 * state.itemPrice).toFixed(2)));
+      state.shippingPrice = 3000;
+      // state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
       state.totalPrice = addDecimals(
-        Number(state.itemPrice) +
-          Number(state.shippingPrice) +
-          Number(state.taxPrice),
+        Number(state.itemsPrice) + Number(state.shippingPrice),
+        //세금
+        // + Number(state.taxPrice),
       );
       Cookies.set("cart", JSON.stringify(state));
     },
     saveShippingAddress: (state, action) => {
       state.shippingAddress = action.payload;
+      Cookies.set("cart", JSON.stringify(state));
+    },
+    savePaymentMethod: (state, action) => {
+      state.paymentMethod = action.payload;
       Cookies.set("cart", JSON.stringify(state));
     },
     hideLoading: (state) => {
@@ -73,7 +90,12 @@ const cartSlice = createSlice({
   },
 });
 
-export const { addToCart, removeFromCart, saveShippingAddress, hideLoading } =
-  cartSlice.actions;
+export const {
+  addToCart,
+  removeFromCart,
+  saveShippingAddress,
+  savePaymentMethod,
+  hideLoading,
+} = cartSlice.actions;
 
 export default cartSlice.reducer;
