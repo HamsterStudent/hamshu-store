3f7f0ffc0739f8c7977ca8a4575f7e0ae68546b4
HamsterStudent <hwanyamyu@gmail.com>
2023-11-29 17:39:19 +0900

    :construction: Temp : temp file

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,68 +1,44 @@
-import { NextResponse } from "next/server";
+import { db } from "@/firebase";
+import axios from "axios";
+import { addDoc, collection, getDocs, query } from "firebase/firestore";
 
-const prodList = [
-  {
-    id: "1",
-    name: "햄슈 에어팟",
-    img: "/assets/prodimg/airpodcase.png",
-    price: 30,
-    countInStock: 0,
-    rating: 4.5,
-    numReviews: 10,
-    description: "so cute",
-  },
-  {
-    id: "2",
-    name: "햄슈 카드홀더",
-    price: 1362,
-    img: "/assets/prodimg/cardholder_01.png",
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 10,
-    description: "so cute",
-  },
-  {
-    id: "3",
-    name: "햄슈 포토홀더",
-    price: 39,
-    img: "/assets/prodimg/photoholder_01.png",
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 10,
-    description: "so cute",
-  },
-  {
-    id: "4",
-    name: "햄슈 접시",
-    price: 100,
-    img: "/assets/prodimg/plate.png",
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 10,
-    description: "so cute",
-  },
-  {
-    id: "5",
-    name: "햄슈 그릇",
-    price: 100,
-    img: "/assets/prodimg/bowl.png",
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 10,
-    description: "so cute",
-  },
-  {
-    id: "6",
-    name: "햄슈 인형",
-    price: 100,
-    img: "/assets/prodimg/stuffedham.png",
-    countInStock: 10,
-    rating: 4.5,
-    numReviews: 10,
-    description: "so cute",
-  },
-];
+export async function POST(req: Request, res: Response) {
+  const request = await req.json();
+  const { merchant_uid, amount } = request;
+  const doc = await addDoc(collection(db, "payment"), {
+    createdAt: Date.now(),
+    merchant_uid: merchant_uid,
+    dbAmount: amount,
+    status: "ready",
+  });
+  try {
+    // 액세스 토큰(access token) 발급 받기
+    const getToken = await axios({
+      url: "https://api.iamport.kr/users/getToken",
+      method: "post", // POST method
+      headers: { "Content-Type": "application/json" },
+      data: {
+        imp_key: `${process.env.API_KEY}`, // REST API 키
+        imp_secret: `${process.env.API_SECRET}`, // REST API Secret
+      },
+    });
 
-export async function GET(req: Request) {
-  return NextResponse.json(prodList);
+    const { access_token } = getToken.data.response; // 인증 토큰
+
+    const getData = await axios({
+      url: `https://api.iamport.kr/payments/prepare?_token=${access_token}`,
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      data: {
+        merchant_uid: merchant_uid,
+        amount: amount,
+      },
+    });
+    const data = getData.data.response;
+    return Response.json({ ...data, status: 200 });
+  } catch (error) {
+    return Response.json("결제를 취소합니다.", {
+      status: 500,
+    });
+  }
 }
