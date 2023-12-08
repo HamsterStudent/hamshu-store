f0def99c58e233de905b4cf7141f386a2b2b1579
HamsterStudent <hwanyamyu@gmail.com>
2023-12-08 19:07:37 +0900

    :fire: Remove : tab component organizing

---
+++ src/app/_components/mainTap.tsx
@@ -0,0 +1,48 @@
+import Image from "next/image";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Info } from "./mainComponentsStyle";
+import ProdList from "./prodList";
+
+export default function MainTap({ dataName }: { dataName: string }) {
+  const [prodData, setProdData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, [dataName]);
+  const fetchData = async () => {
+    try {
+      const response = await axios.post("/api/product", {
+        query: `
+        query {
+          productsCate(tag: ["${dataName}"]) {
+            id
+            img
+            name
+            price
+            tag
+          }
+        }
+      `,
+      });
+      const {
+        data: {
+          data: { productsCate },
+        },
+      } = response;
+      setProdData(productsCate);
+    } catch (error) {
+      console.error("hamsterProd:", error);
+    }
+  };
+  return (
+    <>
+      <Info>
+        www.햄슈그릇에 먹으면 건강해지고 더 맛있어집니다. 진짜임.co.kr
+      </Info>
+
+      <ProdList data={prodData} />
+    </>
+  );
+}
