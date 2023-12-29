a34d6e69f7256fdc4f41d4dc8b3568ed66598f61
HamsterStudent <hwanyamyu@gmail.com>
2023-12-29 14:30:40 +0900

    :bug: Fix : fixing build error

--- src/app/(route)/login/page.tsx
+++ src/app/(route)/login/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { Error, Input, Switcher, Title } from "@/_style/registerStyle";
 import { auth } from "@/firebase";
 import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
@@ -8,53 +9,6 @@ import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 450px;
-  padding: 50px 70px;
-`;
-
-export const Title = styled.h1`
-  font: 42px;
-`;
-
-export const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-export const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-export const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
-
-export const Switcher = styled.span`
-  margin-top: 20px;
-  a {
-    color: #1d9bf0;
-  }
-`;
 export default function Login() {
   const [isLoading, setIsLoading] = useState(false);
   const [email, setEmail] = useState("");
--- src/app/(route)/register/page.tsx
+++ src/app/(route)/register/page.tsx
@@ -5,53 +5,14 @@ import { FirebaseError } from "firebase/app";
 import { auth } from "@/firebase";
 import styled from "styled-components";
 import Link from "next/link";
-export const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 450px;
-  padding: 50px 70px;
-`;
-
-export const Title = styled.h1`
-  font: 42px;
-`;
-
-export const Form = styled.form`
-  margin-top: 50px;
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  width: 100%;
-`;
-
-export const Input = styled.input`
-  padding: 10px 20px;
-  border-radius: 50px;
-  border: none;
-  width: 100%;
-  font-size: 16px;
-  &[type="submit"] {
-    cursor: pointer;
-    &:hover {
-      opacity: 0.8;
-    }
-  }
-`;
-
-export const Error = styled.span`
-  font-weight: 600;
-  color: tomato;
-`;
-
-export const Switcher = styled.span`
-  margin-top: 20px;
-  a {
-    color: #1d9bf0;
-  }
-`;
+import {
+  Error,
+  Form,
+  Input,
+  Switcher,
+  Title,
+  Wrapper,
+} from "@/_style/registerStyle";
 
 export default function CreateAccount() {
   const [isLoading, setIsLoading] = useState(false);
